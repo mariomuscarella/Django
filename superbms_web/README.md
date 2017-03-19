1ae329133d91a069f3d1853e2b34c40dc4a72475
Mario Muscaerlla <mario.muscarella@gmail.com>
2017-03-19 15:22:58 -0400

    Add files via upload

---
+++ superbms_web/README.md
@@ -0,0 +1,2 @@
+user: test
+PW: superbms
---
+++ superbms_web/src/posts/migrations/0003_auto_20160105_2205.py
@@ -0,0 +1,26 @@
+from __future__ import unicode_literals
+
+import datetime
+from django.db import migrations, models
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('posts', '0002_post_user'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='post',
+            name='draft',
+            field=models.BooleanField(default=False),
+        ),
+        migrations.AddField(
+            model_name='post',
+            name='publish',
+            field=models.DateField(default=datetime.datetime(2016, 1, 5, 22, 5, 13, 37350, tzinfo=utc)),
+            preserve_default=False,
+        ),
+    ]
